801f71ec0a150723f0d29935699b89019c7563ea
Hugo Bergon <hugobergon@hotmail.fr>
2020-02-07 16:11:42 +0100

    all in one class

--- projet/app/src/main/java/com/example/editionimage/DefaultPackage/imagehandling/BasicFilter.java
+++ projet/app/src/main/java/com/example/editionimage/DefaultPackage/imagehandling/BasicFilter.java
@@ -5,8 +5,7 @@ import android.graphics.Color;
 
 import java.util.Random;
 
-public class ColorEffect {
-
+public class BasicFilter {
 
     /**
      * Put an image into grey scale on given image
@@ -57,7 +56,7 @@ public class ColorEffect {
      * @param color the color defining the interval's center (H value from 0 to 360)
      * @param range the range defining the interval's width
      */
-    public static void keepColor(BitmapPlus bmp, int color, int range) {
+    public void keepColor(BitmapPlus bmp, int color, int range) {
         color = (color % 360);
         int w = bmp.getWidth();
         int h = bmp.getHeight();
@@ -77,5 +76,74 @@ public class ColorEffect {
         }
         bmp.setHSVPixels(tabs);
     }
-}
 
+
+    /*à  ajouter les augmentations de contrastes et diminution de contrastes*/
+
+
+    /**
+     * Apply a linear transformation of Histogram on the given image (for all type of images)
+     * @param bmp the given image
+     */
+    public static void contrastLinearColor(BitmapPlus bmp){
+        int w = bmp.getWidth();
+        int h = bmp.getHeight();
+        int size = w*h;
+
+        float[][] tabs = bmp.getHSVPixels(h, w);
+
+        int[] hist = new int[101];
+        bmp.ToHistHSV(bmp,hist);
+        int min = 0, max = 100;
+        boolean b = true;
+
+        for(int i=0; i<101; i++) {
+            if (hist[i] != 0 && b) {
+                min = i;
+                b=false;
+            }
+        }
+
+        for(int i=min+1; i<101;i++){
+            if(hist[i]!=0)
+                max=i;
+        }
+
+        for(int i=0; i<size; i++){
+            tabs[2][i] = ((100/(max-min))*(tabs[2][i]*100)-min)/100;
+        }
+        bmp.setHSVPixels(tabs);
+    }
+
+    /**
+     * Apply an equalisation of Histogram on the given image (for all type of images)
+     * @param bmp the given image
+     */
+    public static void contrastEqualColor(BitmapPlus bmp){
+        int w = bmp.getWidth();
+        int h = bmp.getHeight();
+        int size = w*h;
+
+        float[][] tabs = bmp.getHSVPixels(h, w);
+
+        /* faire une seule fonction pour ces lignes*/
+
+        int[] hist = new int[101];
+        bmp.ToHistHSV(bmp,hist);
+
+        int[] C = new int[101];
+        //import
+        bmp.histToCumul(hist,C);
+
+        /* ----------------- */
+
+        for(int i =0; i<size; i++){
+            //voir si c est possible de réduire le nombre de conversion int/float
+            tabs[2][i] = (((float)C[(int)(tabs[2][i]*100)])*100)/(float)(size*100);
+        }
+        bmp.setHSVPixels(tabs);
+    }
+
+
+
+}
