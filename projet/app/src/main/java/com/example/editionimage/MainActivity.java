3cf2e87c459804231df78896af2f70036188e2a3
Hugo Bergon <hugobergon@hotmail.fr>
2020-02-28 16:29:39 +0100

    galery impossible

--- projet/app/src/main/java/com/example/editionimage/MainActivity.java
+++ projet/app/src/main/java/com/example/editionimage/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //usedImage.save();
+                usedImage.saveImage();
             }
         });
 
